6018838635dd8f3fcff4fbd3198bed9f769feb03
riyaagar <agar.riya62@gmail.com>
2020-01-22 19:09:23 -0500

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Riya Agarwal on 1/22/20.
+//  Copyright © 2020 RAgarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
