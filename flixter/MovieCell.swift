1c3fb5f052565a81db8616ad18c1e87b97a66dd8
riyaagar <agar.riya62@gmail.com>
2020-01-26 23:07:03 -0500

    flixter: movie poster, synopsis, title

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Riya Agarwal on 1/26/20.
+//  Copyright © 2020 RAgarwal. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
