699428d175f706c6373145d65f66188d560826aa
riyaagar <agar.riya62@gmail.com>
2020-01-27 18:42:05 -0500

    the url was bd not db, pictures added

--- flixter/MoviesViewController.swift
+++ flixter/MoviesViewController.swift
@@ -34,7 +34,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 self.movies = dataDictionary["results"] as! [[String:Any]]
                 
                 self.tableView.reloadData()
-                print(dataDictionary)
+//                print(dataDictionary)
                 // TODO: Get the array of movies
                 // TODO: Store the movies in a property to use elsewhere
                 // TODO: Reload your table view data
@@ -56,9 +56,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
-        let baseUrl = "https://image.tmbd.org/t/p/w185"
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterURL = URL(string: baseUrl + posterPath)
+        
+        print(posterURL)
         
         cell.posterView.af_setImage(withURL: posterURL!)
         return cell
